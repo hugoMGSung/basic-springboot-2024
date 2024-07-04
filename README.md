4276b5ee772d82399b8878c361b8057e42c477a5
Hugo MG Sung <personar95@naver.com>
2024-07-04 16:20:31 +0900

    16일차 완료

--- README.md
+++ README.md
@@ -887,36 +887,38 @@ Java 빅데이터 개발자과정 Spring Boot 학습 리포지토리
 
 ## 16일차
 - Spring Boot React연동 프로젝트 개발 계속
+	0. npm -> node에서 만든 package manager
+		- yarn, brew(MacOS), chocolatey...
+		- node, npm, npx... 명령어 먼저 공부 
+	
 	1. 로그인
+		- (Front) layout/Header.js 로그인, 회원가입 버튼으로 변경
+		- Login.js 화면 수정
+		- (Back) RestMemberController.java 생성 login() Post메서드 작성
+		- MemberService.java, getMemberByUsernameAndPassword() 메서드 작성
+		- Postman에서 테스트
+		- (Front) Login.js axios 부분 작성
+		- Home.js, localStorage 사용해서 로그인정보 출력
+		- Header.js 로그인버튼 -> 로그아웃
 
 	2. 상세화면 완료
+		- (Back) RestBoardController.java detail() 리턴값 변경
+		- (Front) BoardDetail.js에서 axios 로 가져오기
 
-	3. 입력화면
-
-
-
-
-
-
-
-
-
-
-	3. 구글 로그인
+## 남은것
+- 추가 개발필요
+	1. (Front) 입력화면
+	2. (Back) 구글 로그인
 		- https://console.cloud.google.com/ 구글클라우드 콘솔
 		- 프로젝트 생성
 		- OAuth 동의화면 설정
 		- 개발 계속...
 
-
-## 계속
-- Spring Boot JPA 프로젝트
-	1. 남은 것
+	3. (Back) 포트변경 
 		- 8080 -> 80 서버
-		- http -> https 변경
-
-	- 파일업로드 - AWS S3 체크
-	- 로그인한 사용자 헤더에 표시
+	4. (Back) Https 사용
+	5. (Back) 파일업로드
+	6. 로그인한 사용자 헤더에 표시
 
 
 
--- spring03/frontboard/src/layout/Header.js
+++ spring03/frontboard/src/layout/Header.js
@@ -3,6 +3,20 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate(); // Hook함수는 직접 사용불가(X)
+
+    function gotoLogin() {
+        navigate('/login');
+    }
+
+    function logout() {
+        localStorage.setItem("username", "");
+        localStorage.setItem("email", "");
+        localStorage.setItem("mid", "");        
+        localStorage.setItem("role", "");        
+        localStorage.setItem("loginDt", "");
+        window.location.replace("http://localhost:3000/home"); // URL 재시작
+    }
 
     // return은 화면을 그리겠다.
     return (
@@ -21,8 +35,16 @@ const Header = () => {
                 </ul>
 
                 <div className="col-md-3 text-end me-3">
-                    로그인
-                    회원가입
+                    {localStorage.getItem("username") != "" ? (
+                        <button type='button' className='btn btn-outline-primary'
+                                onClick={logout}>로그아웃</button>
+                    ) : (
+                        <>
+                        <button type='button' className='btn btn-outline-primary me-2' 
+                                onClick={gotoLogin}>로그인</button>
+                        <button type='button' className='btn btn-primary'>회원가입</button>
+                        </>
+                    )}
                 </div>
             </header>
         </div>
